bd3a9c62d645c07e37bab24ebd3032f6a2bc2f4c
Ingrid'G <Ingrid'G@LAPTOP-C8JAC7JQ>
2017-10-12 16:40:29 +0200

    12/10/17

--- vues/v_vueFrais.php
+++ vues/v_vueFrais.php
@@ -3,20 +3,23 @@
         <fieldset>
             <legend>Eléments forfaitisés</legend>
             <?php
-                foreach( $lesFraisForfais as $unFrais) 
-		{
-                        $idFrais = $unFrais['idfrais'];
-                        $libelle = $unFrais['libelle'];
-			$quantite = $unFrais['quantite'];
-                            
-                }
+                foreach ($lesFraisForfait as $unFrais)
+                {
+                    $idFrais = $unFrais['idfrais'];
+                    $libelle = $unFrais['libelle'];
+                    $quantite = $unFrais['quantite'];
             ?>
-            <p>
-		<label for="idFrais"><?php echo $libelle ?></label>
-		<input type="text" id="idFrais" name="lesFrais[<?php echo $idFrais?>]" size="10" maxlength="5" value="<?php echo $quantite?>" >
-            </p>
-            
-            
+                <p>
+                    <label for="idFrais"><?php echo $libelle ?></label>
+                    <input type="text" id="idFrais" name="lesFrais[<?php echo $idFrais?>]" size="10" maxlength="5" value="<?php echo $quantite?>" >
+                </p>
+
+            <?php
+                    }
+            ?>
+
+
+			
          </fieldset>
         <div class="piedForm">
             <p>
@@ -27,4 +30,3 @@
         </div>
     </form>
 </div>
-  
